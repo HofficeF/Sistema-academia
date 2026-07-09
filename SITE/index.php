afd02ac9a8a7048411a5c6f595bcb2cd2332caa3
Felpss1909 <felipe.biondo@aluno.senai.br>
2026-07-09 15:23:04 -0300

    Create index.php

---
+++ SITE/index.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+	<link rel="icon" href="img/icone.png">
+	<title>Login</title>
+	<meta charset="utf-8">
+</head>
+	<link href="css/index.css" rel="stylesheet">
+<body style="background: #4a4a4a;">
+<center>
+	<br>
+	<br>
+	<br>
+	<h1 style="color: #72c704; font-size: 70px;">Login</h1>
+
+	<div id="fundo">
+		<label class="txt">Usuário:</label>
+		<br>
+		<input type="text" name="usuario" placeholder="Digite seu Usúario" class="i">
+		<br>
+		<br>
+		<br>
+		<br>
+		<label class="txt2">Senha:</label>
+		<br>
+		<input type="password" name="senha" placeholder="Digite sua Senha" class="i">
+		<br>
+		<input type="submit" class="b"></input>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<p>Não tem uma conta? <a href="cadastro.php">Cadastre-se!</a></p>
+	</div>
+
+
+
+
+
+
+
+</center>
+</body>
+<!doctype html>
+<html lang="pt-br"
+<head>
+
+	
+	<meta charset="utf-8">
+</head>
+
+<body>
+<center>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</center>
+</body>
