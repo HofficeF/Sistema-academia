b5b5e4697a6e29031a6824604329a2a50d7ea185
Felpss1909 <felipe.biondo@aluno.senai.br>
2026-07-10 13:47:22 -0300

    asdasdda

--- SITE/index.php
+++ SITE/index.php
@@ -1,2 +1,11 @@
-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <title>Login</title>
+    <link rel="icon" href="icone.png" type="image/png">
+</head>
+<body>
+
+</body>
+</html>
