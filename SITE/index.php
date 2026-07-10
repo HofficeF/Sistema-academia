3ad3f113241f2ed2a1f5b71321f8d573e868bc1b
Felpss1909 <felipe.biondo@aluno.senai.br>
2026-07-10 14:44:20 -0300

    Tela Login

--- SITE/index.php
+++ SITE/index.php
@@ -3,9 +3,36 @@
 <head>
     <meta charset="utf-8">
     <title>Login</title>
-    <link rel="icon" href="icone.png" type="image/png">
+    <link rel="icon" href="img/icone.png">
+    <link rel="stylesheet" href="css/index.css">
 </head>
-<body>
+<body style="background: #524f4f;">
+<center>
+    <h1 class="titulo">Login</h1>
+    <br>
+    <br>
+    <br>
+    <div id="fundo">
+    <label class="t1">Usuário:</label>
+    <br>
+    <input type="text" name="usuario" placeholder="Digite seu Usuário" class="i">
+    <br>
+    <br>
+    <br>
+    <label class="t2">Senha:</label>
+    <br>
+    <input type="password" name="senha" placeholder="Digite sua Senha" class="i">
+    <br>
+    <br>
+    <br>
+    <input type="submit" value="Entrar" class="b">
 
+
+    </div>
+
+
+
+
+</center>
 </body>
 </html>
